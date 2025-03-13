4e078801399ac10ed228bd08e288c48773b233d7
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:45:33 -0400

    Description for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,37 @@
+'''
+Question 3
+Given an arbitrary sequence of numerical values, write a function that
+"analyzes" the sequence by generating print outputs of:
+
+number of elements
+number of unique elements
+the min
+the max
+the mean
+the standard deviation
+all the modes (if there are more than one)
+the 25th, 50th, and 75th percentiles
+For example, given this list of numbers:
+
+data = [
+    61, 35, 99, 100, 75, 94, 88, 14, 21, 39, 53, 25, 87, 84,
+    81, 55, 86, 18, 69, 44, 16, 33, 66, 52, 70, 52, 95, 45,
+    94, 35, 68, 70, 52, 53, 30, 87, 79, 51, 92, 72, 55, 40,
+    15, 74, 86, 87, 91, 70, 45, 37
+]
+For calculations that result in floats, display only 3 digits after the
+decimal point for those result.
+
+Your function should print information such as this to the console:
+
+count: 50
+unique count: 38
+min: 14
+max: 100
+mean: 60.800
+std dev: 25.283
+modes: [87, 52, 70]
+25th percentile: 39.750
+median: 63.500
+75th percentile: 86.000
+'''
